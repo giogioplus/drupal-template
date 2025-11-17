6701d1004c481e9bfba46d7027d8651401e0d2a7
giogioplus <giovanna.aulenti@amm.units.it>
2025-11-17 07:04:12 +0000

    fix type filter ckeditor

--- drupal/web/modules/custom/miomodulo/src/Plugin/Block/MioBlocco.php
+++ drupal/web/modules/custom/miomodulo/src/Plugin/Block/MioBlocco.php
@@ -1,25 +1,25 @@
 <?php
 namespace Drupal\miomodulo\Plugin\Block;
 
-use Drupal\Core\Block\Blockbase;
+use Drupal\Core\Block\BlockBase;
 
 
 /**
  * Mio Blocco Custom
- * @Block{
+ * @Block(
  *   id = "mio_blocco_custom",
  *   admin_label = @Translation("Mio blocco custom"),
  *   category = @Translation("Corso Drupal"),
- * }
+ * )
  */
 
 class MioBlocco extends BlockBase {
 
     public function build() {  //per il blocco
       $now = \Drupal::service('date.formatter')->format(time(),'custom','d/m/Y H:i');
-      dump($now);
+      //dump($now);
       return [
-        '#markup' => "<h2>mio blocco custom </h2>",
+        '#markup' => "<h2>mio blocco custom </h2><br/> ora sono le ".$now,
       ];
     }
 }
--- drupal/web/modules/custom/miomodulo/src/Plugin/Filter/MioFiltro.php
+++ drupal/web/modules/custom/miomodulo/src/Plugin/Filter/MioFiltro.php
@@ -6,18 +6,18 @@ use Drupal\filter\FilterProcessResult;
 
 /**
  * Mio filtro Custom
- * @Filter{
+ * @Filter(
  *   id = "mio_filtro_custom",
  *   title = @Translation("Mio filtro custom"),
  *   category = @Translation("Corso Drupal"),
- *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
- * }
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
+ * )
  */
 
 class MioFiltro extends FilterBase {
     public function process ($text, $langcode){ //per il filtro
         $miastringa = $text ?? '';
-        $miastringa = str_replace('[hi]','<strong>Ciao dal mio filtro</strong>', $miastringa);
+        $miastringa = str_replace('[hi]','<h3>Ciao dal mio filtro</h3>', $miastringa);
         return new FilterProcessResult($miastringa);   
     }
 }
